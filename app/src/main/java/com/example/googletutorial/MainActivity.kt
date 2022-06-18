356c787659928951a883cc77a241a6bc82ec2fdc
Mausham Shrestha <mausham.shrestha.01@gmail.com>
2022-06-19 02:51:00 +0930

    Implemented new color, added new Icon and imrpove design

--- app/src/main/java/com/example/googletutorial/MainActivity.kt
+++ app/src/main/java/com/example/googletutorial/MainActivity.kt
@@ -1,15 +1,13 @@
 package com.example.googletutorial
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 import com.example.googletutorial.databinding.ActivityMainBinding
 import java.text.NumberFormat
-import kotlin.math.round
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,20 +20,26 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun calculateTip() {
-        val stringInTextField = binding.costOfService.text.toString()
-        val cost = stringInTextField.toDouble()
+        val stringInTextField = binding.costOfServiceEditText.text.toString()
+        val cost = stringInTextField.toDoubleOrNull()
+        if(cost == null || cost == 0.0) {
+            displayTip(0.0)
+            return
+        }
         val tipPercentage = when (binding.tipOptions.checkedRadioButtonId) {
             R.id.option_twenty_percent -> 0.20
             R.id.option_eighteen_percent -> 0.18
             else -> 0.15
         }
         var tip = tipPercentage * cost
-        val roundUp = binding.roundUpSwitch.isChecked
-
-        if(roundUp) {
+        if(binding.roundUpSwitch.isChecked) {
             tip = kotlin.math.ceil(tip)
         }
 
+        displayTip(tip)
+    }
+
+    private fun displayTip(tip:Double) {
         val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
         binding.tipResult.text = getString(R.string.tip_amount, formattedTip)
     }
